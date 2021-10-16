7e327e3eed73b241282a252c7ac6ad99bf433de5
griitch <omargrinat9@gmail.com>
2021-10-16 01:10:54 +0100

    added login endpoint

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const postsRouter = require("./routes/posts");
 const commentsRouter = require("./routes/comment");
 const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+
 const mongoose = require("mongoose");
 
 //passport stuff
@@ -24,25 +26,9 @@ app.use(express.json());
 app.use(passport.initialize());
 
 app.use("/posts", postsRouter);
-app.use("/comment", commentsRouter);
+app.use("/login", loginRouter);
+app.use("/comments", commentsRouter);
 app.use("/users", usersRouter);
-
-// dummy protected route to test auth
-
-app.post("/profile", (req, res) => {
-  passport.authenticate("jwt", (err, user) => {
-    if (!user) res.send("not auth");
-    else {
-      res.json(user);
-    }
-  })(req, res);
-});
-
-app.post("/login2", (req, res) => {
-  let { username } = req.body;
-  const token = jwt.sign({ username }, process.env.secret);
-  res.json({ message: "auth good", token });
-});
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
---
+++ routes/login.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const Router = express.Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const { secret } = process.env;
+
+/*
+@route : POST /login
+@desc : login and return the jwt if authentified
+@access : public
+*/
+Router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username: username });
+
+    if (!user) res.json({ error: true, message: "No such userame exists" });
+
+    const isMatching = await bcrypt.compare(password, user.passwordHash);
+    if (isMatching) {
+      const token = jwt.sign(username, secret, {});
+      res.json({ token, message: "authentication passed" });
+    } else {
+      res.json({ error: true, message: "wrong password" });
+    }
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+module.exports = Router;
